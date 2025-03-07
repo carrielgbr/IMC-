3843542ec4d6e6bf4ef168a597e84c3dc7058053
carrielgbr <gabrielcarriel61@gmail.com>
2025-03-07 00:03:30 -0300

    Vai Corinthians

---
+++ teste.py
@@ -0,0 +1,17 @@
+name = input ('Qual é o seu nome ?')
+
+print ('Olá, bem vindo !', name)
+
+print ('Vamos fazer algumas, sobre voce e sua vida, certo ?')
+
+peso = input ('Quanto voce pesa ?')
+
+peso_int = int (peso)
+
+altura = input ('Qual a sua altura ?')
+
+altura_int = int (altura )
+
+\\total = ((peso_int / altura_int) * altura_int)
+
+\\print (total)
