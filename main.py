9abe6626154a24cb018b214e5d3600def9c66b2b
carrielgbr <gabrielcarriel61@gmail.com>
2025-03-08 22:31:58 -0300

    concluido o código do IMC

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ Test = "Hello World!"
 
 print("Teste: ", Test)
 
-Altura = 1.50   # Em metros
-Peso = 75       # Em Quilogramas
+Altura = float (input ("insira sua altura (EM METROS COM . PONTO)"))# Em metros
+Peso = float (input ("insira seu pelo"))       # Em Quilogramas
 
 # Calculo do IMC
 IMC = (Peso / Altura ** 2)
 
+teste = ("Categorias de IMC: Menor que 18,5 → Baixo Peso 18,5 – 24,9 → Peso Normal 25 – 29,9 → Excesso de Peso Maior que 30 → Obesidade Maior que 35 → Obesidade Extrema")
+
 # Resultado. Float configurado para mostrar 2 casas decimais.
-print(f"IMC: {IMC:.2f}")
+
+print(f"IMC: {IMC:.2f}\n"  + teste)
